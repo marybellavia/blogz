c62c2ca27f5d8510ab1217226a5f2617d8e141fd
Mary Bellavia <sauerwein.mary@gmail.com>
2019-11-26 14:22:59 -0600

    got the database correctly linked to html/python. Added the add-blog function to add to database.

--- main.py
+++ main.py
@@ -9,19 +9,39 @@ db = SQLAlchemy(app)
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    blog_title = db.Column(db.String(120))
-    blog_content = db.Column(db.String(255))
+    title = db.Column(db.String(120))
+    content = db.Column(db.String(120)) # TODO update this to be a longer amount of characters for blog content
 
-    def __init__(self,blog_title):
-        self.blog_title = blog_title
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+def get_blogs():
+    return Blog.query.all()
 
 @app.route('/add-blog', methods=['POST', 'GET'])
 def add_blog():
-    return render_template('add_blog.html', title="Add a Blog Entry")
+    if request.method == 'POST':
+        # TODO add error messages for if they leave either field blank
 
-@app.route('/', methods=['POST', 'GET'])
+        new_blog_title = request.form['blog-title']
+        new_blog_content = request.form['blog-content']
+        new_blog = Blog(new_blog_title, new_blog_content)
+        db.session.add(new_blog)
+        db.session.commit()
+        
+        # TODO create redirect to new page displaying blog via blog id?
+
+    
+    return render_template('add-blog.html', title="Add a Blog Entry")
+
+@app.route('/blog', methods=['POST', 'GET'])
+def blog():
+    return render_template('blog.html', title="Build a Blog", bloglist=get_blogs())
+
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('blog.html', title="Build a Blog")
+    return render_template('blog.html', title="Build a Blog", bloglist=get_blogs())
 
 if __name__ == '__main__':
     app.run()
