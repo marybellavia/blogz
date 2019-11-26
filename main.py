6f5822802dfd98daa57884c6b529036da89e598b
Mary Bellavia <sauerwein.mary@gmail.com>
2019-11-26 07:39:56 -0600

    made main page and linked to database

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    blog_title = db.Column(db.String(120))
+
+    def __init__(self,blog_title):
+        self.blog_title = blog_title
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template('blog.html', title="Build a Blog")
+
+if __name__ == '__main__':
+    app.run()
