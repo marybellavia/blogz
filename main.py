05b0d940bedd29243a35712b1e7f0dfe07556c6d
Mary Bellavia <sauerwein.mary@gmail.com>
2019-12-03 14:58:02 -0600

    added User class and created relationship between blog and user classes

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 # importing required modules
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 # setting up Flask and linking to database
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-# creating Blog object to create database entries -- title and body
+# creating Blog class to create database entries -- need title, body, owner
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(1000))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
+# creating User class to create database entries -- need username and password
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    password = db.Column(db.String(30))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 # helper function to query database for all blogs
 def get_blogs():
     return Blog.query.all()
@@ -29,7 +41,8 @@ def newpost():
     if request.method == 'POST':
         new_blog_title = request.form['blog-title']
         new_blog_body = request.form['blog-body']
-        new_blog = Blog(new_blog_title, new_blog_body)
+        owner = User.query.filter_by(email=session['email']).first()
+        new_blog = Blog(new_blog_title, new_blog_body, owner)
         # conditionals to insert error messages if the form is left blank
         if new_blog_title == '':
             title_error = 'Please fill in the title'
@@ -44,6 +57,7 @@ def newpost():
     # returns the blank form on GET request
     return render_template('newpost.html', title="Add a Blog Entry", title_error=title_error, body_error=body_error)
 
+@app.route('/')
 @app.route('/blog', methods=['POST', 'GET'])
 def blog():
     # getting id parameter from GET request, if there is an id parameter
@@ -58,10 +72,6 @@ def blog():
     else:
         return render_template('blog.html', title="Build a Blog", bloglist=get_blogs())
 
-# made this as the home page? could delete?
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('blog.html', title="Build a Blog", bloglist=get_blogs())
 # makin that shit run!
 if __name__ == '__main__':
     app.run()
